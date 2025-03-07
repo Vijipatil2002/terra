364ac2d9dc7051a6ab1bce3e5a4c4ec50df92b07
Vijipatil2002 <136726706+Vijipatil2002@users.noreply.github.com>
2025-03-07 11:58:57 +0530

    Update cd.groovy

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -5,12 +5,16 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'My_id', url: 'https://github.com/Vijipatil2002/Hospital_frontend_project.git']])
+                script {
+                        git branch: branch, 
+                            credentialsId: credentialId, 
+                            url: scmUrl
+                    }
             }
         }
         stage('deploy') {
             steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'My_id', url: 'https://github.com/Vijipatil2002/Hospital_frontend_project.git']])
+                echo " deploy"
             }
         }
     }
