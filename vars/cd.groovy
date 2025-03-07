958cfb362d117bdc3290bd585fb21ef4aaec3eec
Vijipatil2002 <136726706+Vijipatil2002@users.noreply.github.com>
2025-03-07 11:57:23 +0530

    Update cd.groovy

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -1,4 +1,4 @@
-
+def call(String scmUrl, String branch, String credentialId) {
 pipeline {
     agent any
 
@@ -8,5 +8,11 @@ pipeline {
                 checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'My_id', url: 'https://github.com/Vijipatil2002/Hospital_frontend_project.git']])
             }
         }
+        stage('deploy') {
+            steps {
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'My_id', url: 'https://github.com/Vijipatil2002/Hospital_frontend_project.git']])
+            }
+        }
     }
+  }
 }
