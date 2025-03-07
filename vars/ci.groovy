3e6e4f7cf11cc9a67d119b011fa90df3969dd7a9
Vijipatil2002 <136726706+Vijipatil2002@users.noreply.github.com>
2025-03-07 11:58:21 +0530

    Update ci.groovy

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -5,7 +5,11 @@ def call(String scmUrl, String branch, String credentialId) {
         stages {
             stage('Hello') {
                 steps {
-                    checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'My_id', url: 'https://github.com/Vijipatil2002/Hospital_frontend_project.git']])
+                     script {
+                        git branch: branch, 
+                            credentialsId: credentialId, 
+                            url: scmUrl
+                    }
                 }
             }
         }
