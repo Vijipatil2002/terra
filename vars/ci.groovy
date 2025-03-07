8ced471b49089c6da80a5893312254971a385e26
Vijipatil2002 <136726706+Vijipatil2002@users.noreply.github.com>
2025-03-07 11:49:00 +0530

    Create ci.groovy

---
+++ vars/ci.groovy
@@ -0,0 +1,31 @@
+def call(String scmUrl, String branch, String credentialId) {
+    pipeline {
+        agent any
+        tools{
+            maven 'maven'
+        }
+        stages {
+            stage('Checkout') {
+                steps {
+                    script {
+                        git branch: branch, 
+                            credentialsId: credentialId, 
+                            url: scmUrl
+                    }
+                }
+            }
+             stage('Build'){
+                steps {
+                            bat 'mvn clean package'
+                    }
+                    post {
+                    success {
+                        echo 'Archiving the artifacts'
+                        archiveArtifacts artifacts: '**/target/*.war'
+                    }
+                }
+            }
+            
+        }
+    }
+}
